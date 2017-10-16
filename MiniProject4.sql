c4a14c91fb574361445ccc0529c89f7789269384
georgesturrock <george.sturrock@gmail.com>
2017-10-15 21:15:42 -0700

    Add files via upload

--- MiniProject4.sql
+++ MiniProject4.sql
@@ -1,11 +1,15 @@
 ### Q1.1 - All customers who have not purchased bikes or tires ###
-select distinct c.CustomerID, c.CustFirstName, c.CustLastName
+select customers.customerid, customers.custfirstname, customers.custlastname
+from customers
+where customers.customerid not in (
+select distinct o.customerid
 from order_details od
-inner join products p on od.ProductNumber = p.ProductNumber
-inner join orders o on od.OrderNumber = o.OrderNumber
-right join customers c on o.CustomerID = c.CustomerID
-where p.CategoryID not in (2, 6)
-or o.OrderNumber IS NULL;
+inner join products p on od.productnumber = p.productnumber
+inner join orders o on od.ordernumber = o.ordernumber
+inner join customers c on o.customerid = c.customerid
+inner join categories cat on p.categoryid = cat.categoryid
+where cat.categoryid in (2, 6)
+order by o.customerid);
 
 ### Q1.2 - All customers who have purchased a bike, but not a helmet ###
 #https://dev.mysql.com/doc/refman/5.7/en/exists-and-not-exists-subqueries.html
@@ -21,7 +25,7 @@ from order_details
 inner join products on order_details.ProductNumber = products.ProductNumber
 inner join orders on order_details.OrderNumber = orders.OrderNumber
 inner join customers c2 on orders.CustomerID = c2.CustomerID
-where products.CategoryID = 1 and products.ProductName like '%Helmet%'
+where products.CategoryID = 1 and products.ProductName like '%Helmet'
 and c.CustomerID = c2.CustomerID)
 order by c.CustomerID;
 
@@ -38,7 +42,7 @@ from order_details, products, categories
 where order_details.ProductNumber = products.ProductNumber
 and products.CategoryID = categories.CategoryID
 and categories.CategoryID = 1
-and ProductName like '%Helmet%')
+and ProductName like '%Helmet')
 order by OrderNumber;
 
 ### Q1.4 Customer Orders with a bike and a helmet ###
@@ -55,7 +59,7 @@ inner join products p on od.ProductNumber = p.ProductNumber
 inner join orders o on od.OrderNumber = o.OrderNumber
 right join customers c on o.CustomerID = c.CustomerID
 where p.CategoryID = 1
-and p.productname like '%Helmet%')
+and p.productname like '%Helmet')
 order by c.CustomerID, od.OrderNumber;
 
 ### Q1.5 Vendors who sell accessories, car racks and clothing
